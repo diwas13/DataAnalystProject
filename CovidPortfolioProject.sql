2008cecb7606ab3192a9fbf65257482604867896
diwas13 <110383510+diwas13@users.noreply.github.com>
2025-07-24 14:12:41 +0100

    Update CovidPortfolioProject.sql

--- CovidPortfolioProject.sql
+++ CovidPortfolioProject.sql
@@ -1,17 +1,17 @@
+-- Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggrefate Functions, Creating Views, Converting Data Types
 Select *
 FROM PortfolioProject..CovidDeaths$
 Where continent is not null
 order by 3, 4
 
---Select *
---From PortfolioProject..CovidVaccinations$
---order by 3, 4
-
+-- Select Data that we are going to be starting with
+	
 Select Location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProject..CovidDeaths$
 order by 1,2
 
 -- Total Cases vs Total Deaths
+-- Shows likelihood of dying if you contract covid in your country
 
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases) * 100 as DeathPercentage
 From PortfolioProject..CovidDeaths$
